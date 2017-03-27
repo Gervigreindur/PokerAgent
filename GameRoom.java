82249679b70ef620087ea588998d8b9023fae58c
vediserna <vedise13@ru.is>
2017-03-27 18:42:10 +0000

    fyrir gísla+

--- GameRoom.java
+++ GameRoom.java
@@ -7,10 +7,10 @@ public class GameRoom {
 	public static void main(String[] args) {
 		Board board = new Board(8);
 		
-		board.addPlayer(new Human("Gisli", buyIn));
-		board.addPlayer(new Human("Steini", buyIn));
-		//board.addPlayer(new Agent("Vedis", buyIn, board));
-		//board.addPlayer(new Agent("Einir", buyIn, board));
+		//board.addPlayer(new Human("Gisli", buyIn));
+		//board.addPlayer(new Human("Steini", buyIn));
+		board.addPlayer(new Agent("Vedis", buyIn, board));
+		board.addPlayer(new Agent("Einir", buyIn, board));
 		
 		while(board.isActive()) {
 			board.play();
